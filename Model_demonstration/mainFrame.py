0f36b9969389e88ea3f2cfaee38cde3323ceaf3c
SunChuan <sunchuanop@outlook.com>
2018-11-21 22:14:08 +0800

    撰写报告

--- Model_demonstration/mainFrame.py
+++ Model_demonstration/mainFrame.py
@@ -11,8 +11,7 @@ class mainFrame(wx.Frame):
         wx.Frame.__init__(self, None, -1, app_title, app_size)
         panel = wx.Panel(self, -1)
 
-        #self.label_content = ['性别', '年龄', '借记卡数', '贷记卡数', '近1年消费总笔数', '近1年消费总金额', '消费城市数']
-        self.label_content = ['性别', '年龄段', '消费城市数量', '近1年月累计交易金额最大值']
+        self.label_content = ['性别', '年龄', '消费城市数量', '月消费金额最大值']
         self.box_count = 4
         self.box_height = app_size[1] / self.box_count
         self.label_width = 30
@@ -72,30 +71,72 @@ class mainFrame(wx.Frame):
         values = [1.]
         try:
             for input_box in self.input_box_list:
-                value = float(input_box.GetValue())
-                values.append(value)
+                value = input_box.GetValue()
+                if len(values) == 1:
+                    if(value == '男'):
+                        values.append(1.)
+                    elif(value == '女'):
+                        values.append(0.)
+                    else:
+                        self.print_error_message()
+                elif len(values) == 2:
+                    values.append(self.get_age_scope(int(value)))
+                else:
+                    values.append(float(value))
+            
             result = self.reg_ols.predict([values])
-            self.area_text.SetValue('预估车辆价格：' + str(result[0]))
+            self.area_text.SetValue('预估车辆价格：' + str(round(result[0], 4)) + '万')
             self.promote_car(result[0])
         except:
-            self.area_text.SetValue('输入的数值不正确，请输入纯数字，不能为空')
+            self.print_error_message()
 
     def load_car_info(self):
-        car_data = pd.read_csv('car_data', index_col='CarID')
+        car_data = pd.read_csv('car_data.txt', index_col='CarID')
         self.car_values = car_data.values.T.tolist()
+
+    def print_error_message(self):
+        self.area_text.SetValue('输入的数值不正确，请输入纯数字，不能为空')
     
     def promote_car(self, car_price):
+        car_found = False
         for i in range(len(self.car_values[3])):
             if self.car_values[3][i] > car_price:
-                self.area_text.write(self.car_values[0][i] + self.car_values[1][i])
+                car_found = True
+                self.area_text.write('\n' + '推荐车型：')
+                self.area_text.write('\n' + str(int(self.car_values[2][i])) + '款 ' 
+                    + self.car_values[0][i] + self.car_values[1][i] + '，报价：' 
+                    + self.car_values[6][i])
                 break
+        if not car_found:
+            self.area_text.write('\n' + '推荐车型：')
+            self.area_text.write('\n' + str(int(self.car_values[2][-1])) + '款 ' 
+                + self.car_values[0][-1] + self.car_values[1][-1] + '，报价：' 
+                + self.car_values[6][-1])
+    
+    def get_age_scope(self, age):
+        if age >= 0 and age <= 17:
+            return 1
+        elif age > 17 and age <=24:
+            return 2
+        elif age > 24 and age <=31:
+            return 3
+        elif age > 31 and age <=38:
+            return 4
+        elif age > 38 and age <=45:
+            return 5
+        elif age > 45 and age <=52:
+            return 6
+        elif age > 52 and age <=59:
+            return 7
+        elif age > 59:
+            return 8
 
 if __name__ == '__main__':
     title = '模型演示'
     app = wx.App()
 
     frame = mainFrame(title, (600, 300))
-    frame.SetSize(550, 220)
+    frame.SetSize(420, 220)
     frame.SetMaxSize(frame.Size)
     frame.Center()
     frame.Show()
